979fc11ce294c6882f0d6f77fe34f243a9be5ea3
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-08-30 22:45:03 +0100

    Test successfully determine a equilateral

---
+++ Triangle/Models/TriangleTracker.cs
@@ -0,0 +1,27 @@
+namespace Triangle
+{
+  public class TriangleTracker
+  {
+    public bool IsEquilateral(int number1, int number2, int number3)
+    {
+      int number = number1 + number2 + number3;
+      return number / number1 == number2;
+    }
+
+    public bool IsIsosceles(int number1, int number2, int number3)
+    {
+      int no1 = number1 + number2;
+      int no2 = number2 + number3;
+      int no3 = number3 + number1;
+      return no1 > number3 || no3 > number2;
+    }
+
+    public bool IsScalene(int number1, int number2, int number3)
+    {
+      int no1 = number1 + number2;
+      int no2 = number2 + number3;
+      int no3 = number3 + number1;
+      return no1 < number3 || no2 < number1 || no3 < number2 && number1 != number2 || number2 != number3 || number3 != number1;
+    }
+  }
+}
